bae52be0d211b0472f485c33dd9ca247f66a2454
Gevorg Minasian <cool.port-965@yandex.ru>
2023-10-06 03:04:46 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# dayoftheweek = int(input("Введите номер дня недели: "))
+# if dayoftheweek == 1:
+#     print("Понедельник")
+# elif dayoftheweek == 2:
+#     print("Вторник")
+# elif dayoftheweek == 3:
+#     print("Среда")
+# elif dayoftheweek == 4:
+#     print("Четверг")
+# elif dayoftheweek == 5:
+#     print("Пятница")
+# elif dayoftheweek == 6:
+#     print("Суббота")
+# elif dayoftheweek == 7:
+#     print("Воскресенье")
+
+
+# dayofthemonth = int(input("Введите номер месяца в году: "))
+# if dayofthemonth == 1:
+#     print("Январь")
+# elif dayofthemonth == 2:
+#     print("Февраль")
+# elif dayofthemonth == 3:
+#     print("Март")
+# elif dayofthemonth == 4:
+#     print("Апрель")
+# elif dayofthemonth == 5:
+#     print("Май")
+# elif dayofthemonth == 6:
+#     print("Июнь")
+# elif dayofthemonth == 7:
+#     print("Июлю")
+# elif dayofthemonth == 8:
+#     print("Август")
+# elif dayofthemonth == 9:
+#     print("Сентябрь")
+# elif dayofthemonth == 10:
+#     print("Октябрь")
+# elif dayofthemonth == 11:
+#     print("Ноябрь")
+# elif dayofthemonth == 12:
+#     print("Декабрь")
+
+# randomNumber = int(input("Введите число: "))
+# if randomNumber > 0:
+#     print("Number is positive")
+# elif randomNumber < 0:
+#     print("Number is negative")
+# else:
+#     print("Number is equal to zero")
+
+
+# randomNumber1 = int(input("Введите первое число: "))
+# randomNumber2 = int(input("Введите второе число: "))
+# if randomNumber1 == randomNumber2:
+#     print("Числа равны")
+# elif randomNumber1 > randomNumber2:
+#     print(randomNumber1, randomNumber2)
+# elif randomNumber2 > randomNumber1:
+#     print(randomNumber2, randomNumber1)
